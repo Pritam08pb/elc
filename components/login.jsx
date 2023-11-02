fc94abdd85281dfdffc6b1cb203fa3e70ccec6cc
Pritam Prasanjeet Behera <99427618+Pritam08pb@users.noreply.github.com>
2023-11-02 23:00:31 +0530

    Update login.jsx

--- components/login.jsx
+++ components/login.jsx
@@ -1,31 +1,29 @@
-import React from 'react'; // Import React
-import styles from '../styles/login.module.css'
-
-const Login = () => {
+import React from 'react'
+import styles from "../styles/login.module.css"
+function login() {
   return (
-    <div>
-      <div className={styles.background}>
+    <div className={styles.body}>
+       <div className={styles.background}>
         <div className={styles.shape}></div>
         <div className={styles.shape}></div>
       </div>
-      <form>
+    <form id={styles.form}>
         <h3>Login Here</h3>
 
-        <label htmlFor="username">Username</label>
-        <input type="text" placeholder="Email or Phone" id="username" />
+        <label id={styles.label} htmlFor="username">Username</label>
+        <input className={styles.input} type="text" placeholder="Email or Phone" id="username" />
 
-        <label htmlFor="password">Password</label>
-        <input type="password" placeholder="Password" id="password" />
+        <label id={styles.label} htmlFor="password">Password</label>
+        <input className={styles.input} type="password" placeholder="Password" id="password" />
+        <br />   
+        <a href="">Forgot Password ?</a>
 
-        <button>Log In</button>
-        <div className={styles.social}>
-          <div className={styles.go}>
-            <i className={styles.fab}></i> Google
-          </div>
-        </div>
-      </form>
+        <button className={styles.button}>Log In</button>
+        
+        
+    </form>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default login
