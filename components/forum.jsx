a46152592bdc5b3e3a884cd9b6b9bf04a8014fc5
Pritam Prasanjeet Behera <99427618+Pritam08pb@users.noreply.github.com>
2024-01-25 20:30:14 +0530

    forum prototype added
    a chatroom added using "Socket io" ,
     it will further developed in to forum .

--- components/forum.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from '../styles/forum.module.css'
-const forum = () => {
-  return (
-    <div className={styles.full}><h1>Forum</h1></div>
-  )
-}
-
-export default forum
